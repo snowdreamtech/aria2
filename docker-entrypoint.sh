ef7c3d718431bedc3895222f91c8a34b6be4946e
snowdream <sn0wdr1am@qq.com>
2024-06-16 17:03:05 +0800

    feat: aria2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
+# Nginx and AriaNg
+/usr/sbin/nginx -c /etc/nginx/nginx.conf
+
 # aria2c
 /usr/bin/aria2c --conf-path /etc/aria2.conf
-
-# Nginx and AriaNg
-/usr/sbin/nginx -g  "daemon off;" -c /etc/nginx/nginx.conf
 
 # exec commands
 exec "$@"
