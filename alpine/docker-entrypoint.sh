bc4d22322c2d35914a6c2e8d8af5b8bcfa262981
snowdream <sn0wdr1am@qq.com>
2024-09-24 21:24:42 +0800

    feat: aria2 1.37.0 based on alpine and debian image

---
+++ alpine/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+# aria2c
+/usr/bin/aria2c --conf-path /etc/aria2.conf
+
+# exec commands
+if [ -n "$*" ]; then
+    sh -c "$*"
+fi
+
+# keep the docker container running
+# https://github.com/docker/compose/issues/1926#issuecomment-422351028
+if [ "${KEEPALIVE}" -eq 1 ]; then
+    trap : TERM INT
+    tail -f /dev/null & wait
+    # sleep infinity & wait
+fi
