f0ff9e4ffbcfe547599d8357391b3dc65ea66745
snowdream <sn0wdr1am@qq.com>
2025-12-04 08:10:32 +0800

    Update Dockerfiles and entrypoint scripts for Aria2 images on Alpine and Debian
    - Modify OCI annotations in Dockerfiles to reflect Aria2 branding and update documentation links.
    - Add Aria2 configuration files for Alpine and Debian images.
    - Implement entrypoint scripts to initialize and start Aria2 with RPC support.
    - Remove obsolete base initialization scripts.

---
+++ alpine/entrypoint.d/20-aria2-start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "$DEBUG" = "true" ]; then echo "→ [aria2] Starting aria2..."; fi
+
+# openssl rand -base64 33
+if [ -z "${RPC_SECRET}" ]; then {
+    RPC_SECRET=$(openssl rand -base64 33)
+    echo "Generate random aria2 rpc secret: ${RPC_SECRET}"
+}
+fi
+
+# aria2c
+/usr/bin/aria2c --conf-path /etc/aria2.conf --daemon --enable-rpc --rpc-listen-all --rpc-secret="${RPC_SECRET}"
+
+# exec
+
+if [ "$DEBUG" = "true" ]; then echo "→ [aria2] Aria2 started."; fi
